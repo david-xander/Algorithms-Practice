86526626db1672038dd9c4e809d52e2c96f670d4
david-xander <ddelgado@a2sistemas.com>
2021-09-13 12:04:01 +0200

    bubble sort done

--- sorting/bubblesort.py
+++ sorting/bubblesort.py
@@ -1,7 +1,15 @@
 def main():
-    buble
+    print( bubble_sort([54, 26, 93, 17, 77, 31, 44, 55, 20]) )
 
-    pass
+
+def bubble_sort(input:list, ) -> list:
+    n = len(input)
+    for keep_passing in range(n - 1):
+        for i in range(n - 1):
+            if input[i] >= input [i + 1]:
+                input[i], input[i + 1] = input[i + 1], input[i]
+    
+    return input
 
 if __name__ == "__main__":
     # execute only if run as a script
